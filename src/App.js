c148edc5605ab5dcd0142ef57bab13eab94a4b28
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-26 18:53:55 +0530

    map practice

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './App.scss';
 import NameTag from './components/nameTag';
 
 
 
-const makeGreen = BaseComponent => props =>{
-  const addGreen={
-      style:{
-          color:"green"
-      }
-  }
+// const makeGreen = BaseComponent => props =>{
+//   const addGreen={
+//       style:{
+//           color:"green"
+//       }
+//   }
 
-  const newProps={
-      ...props,...addGreen
-  }
+//   const newProps={
+//       ...props,...addGreen
+//   }
 
-  return <BaseComponent{...newProps}/>
-}
+//   return <BaseComponent{...newProps}/>
+// }
 
-const removeInline = BaseComponent =>props =>{
-  const newProps={...props}
-  delete newProps.style
-  return <BaseComponent{...newProps}/>
-}
+// const removeInline = BaseComponent =>props =>{
+//   const newProps={...props}
+//   delete newProps.style
+//   return <BaseComponent{...newProps}/>
+// }
 
 
-const GreenNameTag =makeGreen(NameTag)
-const CleanNameTag=removeInline(NameTag)
+// const GreenNameTag =makeGreen(NameTag)
+// const CleanNameTag=removeInline(NameTag)
+
+
+const initiaName=[
+  {firstname:"peter",lastname:"peterson"},
+  {firstname:"John",lastname:"Johnson"},
+  {firstname:"Jill",lastname:"Jillson"}
+]
 
 
 function App() {
-  
+  const[name,setName]=useState(initiaName)
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='name title'>Names List</h1>
-        <GreenNameTag firstname="peter" lastname="peterson">Peter</GreenNameTag>
-         <CleanNameTag style={{color:'red'}} firstname="John" lastname="Johnson">John</CleanNameTag>{/*that is called as HOC */}
-        <NameTag style={{color:'red'}} firstname="Jill" lastname="Jillson" >Jill</NameTag>
-        <NameTag></NameTag>
+        {
+          name.map((v,i)=>{
+            return <NameTag key={`${i}${v.firstname}${v.lastname}`} firstname={v.firstname} lastname={v.lastname}>John</NameTag>
+          })
+        }
+        {/* <NameTag firstname="peter" lastname="peterson">Peter</NameTag>
+         <NameTag firstname="John" lastname="Johnson">John</NameTag>
+        <NameTag  firstname="Jill" lastname="Jillson" >Jill</NameTag> */}
       </header>
     </div>
   );
