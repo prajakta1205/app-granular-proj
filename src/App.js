e35c772cfd8d016fba6172d0857dfbbcdabb2288
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-26 17:44:50 +0530

    HOC pratice

--- src/App.js
+++ src/App.js
@@ -3,15 +3,42 @@ import './App.scss';
 import NameTag from './components/nameTag';
 
 
+
+const makeGreen = BaseComponent => props =>{
+  const addGreen={
+      style:{
+          color:"green"
+      }
+  }
+
+  const newProps={
+      ...props,...addGreen
+  }
+
+  return <BaseComponent{...newProps}/>
+}
+
+const removeInline = BaseComponent =>props =>{
+  const newProps={...props}
+  delete newProps.style
+  return <BaseComponent{...newProps}/>
+}
+
+
+const GreenNameTag =makeGreen(NameTag)
+const CleanNameTag=removeInline(NameTag)
+
+
 function App() {
-  console.log('hi')
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='name title'>Names List</h1>
-        <NameTag firstname="peter" lastname="peterson">Peter</NameTag>
-        <NameTag firstname="John" lastname="Johnson">John</NameTag>
-        <NameTag firstname="Jill" lastname="Jillson" >Jill</NameTag>
+        <GreenNameTag firstname="peter" lastname="peterson">Peter</GreenNameTag>
+         <CleanNameTag style={{color:'red'}} firstname="John" lastname="Johnson">John</CleanNameTag>{/*that is called as HOC */}
+        <NameTag style={{color:'red'}} firstname="Jill" lastname="Jillson" >Jill</NameTag>
+        <NameTag></NameTag>
       </header>
     </div>
   );
--- src/components/nameTag.js
+++ src/components/nameTag.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 
-const makeGreen = BaseComponent => props =>{
-    
-}
-
 function nameTag(props){
     return(
-    <div className="name">
-    <h3 >First Name:{props.firstname}</h3>
-    <h3 >Last Name:{props.lastname}</h3>
-    </div>
+        <div className="name">
+            <h3 style={props.style}>First Name:{props.firstname}</h3>
+            <h3 style={props.style}>Last Name:{props.lastname}</h3>
+        </div>
     );
 }
 
